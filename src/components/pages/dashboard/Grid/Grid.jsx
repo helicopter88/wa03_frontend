db63075c8d9ff6a80d5cb47efce4a363a89a543b
Abraao <ap2314@ic.ac.uk>
2016-06-01 15:26:46 +0000

    Selling and Buying button quantities are asynchronous now

--- src/components/pages/dashboard/Grid/Grid.jsx
+++ src/components/pages/dashboard/Grid/Grid.jsx
@@ -7,7 +7,8 @@ var Grid = React.createClass({
     return ({
       showModal: false,
       elements: [],
-      quantity: 0,
+      buyQuantity: 0,
+      sellQuantity: [],
       symbol: "",
       isLoading: false,
       ws: new WebSocket("ws://webapps3.westeurope.cloudapp.azure.com:8080/")
@@ -17,9 +18,16 @@ var Grid = React.createClass({
         this.setState({ symbol: e.target.value });
   },
 
-  handleValue(e) {
-	this.setState({ quantity: e.target.value}); 
-},
+  handleBuyValue(e) {
+	this.setState({ buyQuantity: e.target.value}); 
+  },
+
+  handleSellValue(e, elem) {
+    var arr = this.state.sellQuantity;
+    arr[elem] = e.target.value;	
+    this.setState({ sellQuantity: arr});
+
+  },
 
     openModal: function() {
       this.setState({ showModal: true});
@@ -47,7 +55,7 @@ var Grid = React.createClass({
       if(event.data.indexOf("ask_price") > -1) {
 	var msg = JSON.parse(event.data.substring(10));
 	var price = parseFloat(msg.res);
-        this.state.ws.send("db insert_trans " + sessionStorage.userName + " " + this.state.symbol + " " + price + " " + this.state.quantity + " t"); 
+        this.state.ws.send("db insert_trans " + sessionStorage.userName + " " + this.state.symbol + " " + price + " " + this.state.buyQuantity + " t"); 
       }
       if(event.data.indexOf("it_" + sessionStorage.userName) > -1) {
 	
@@ -58,8 +66,10 @@ var Grid = React.createClass({
           alert("Negative price is not allowed");
         } else if (event.data.indexOf("3") > -1) {
           alert("Wrong currency");
+        } else if (event.data.indexOf("4") > -1) {
+          alert("Not enough capital");
         } else if(event.data.indexOf("-1") > -1) {
-          alert("Not enough funds or not enough shares");
+          alert("Not enough shares");
         } else {
 	  this.setState({ showModal:false});
 	  this.state.ws.send("db get_owned " + sessionStorage.userName);
@@ -75,14 +85,14 @@ var Grid = React.createClass({
     },
 
   buyStocks: function() {
-    var quantity = this.state.quantity;
+    var quantity = this.state.buyQuantity;
     var symbol = this.state.symbol;
     this.state.ws.send("yahoo exists " + symbol);
     
   },
 
   sell: function(elem) {
-	this.state.ws.send("db insert_trans " + sessionStorage.userName + " " + elem.instr + " " + elem.bp + " " + this.state.quantity + " f"); 
+	this.state.ws.send("db insert_trans " + sessionStorage.userName + " " + elem.instr + " " + elem.bp + " " + this.state.sellQuantity[elem.name] + " f"); 
   },
   
 
@@ -119,15 +129,25 @@ var Grid = React.createClass({
             <tbody>
             {this.state.elements.map(elem => <tr>
                  <td> {elem.instr} </td>
-                 <td>{elem.name} </td> <td>  {elem.amount} </td> <td> {elem.bp} </td> <td> {elem.ap} </td> <td>  <input type="number" value={this.state.quantity} onChange={this.handleValue}></input><Button bsStyle="danger" bsSize="small" disabled={this.state.isLoading}
-       	  style={{marginLeft: 1 + 'em'}} 
-          onClick={function() {
-			self.setState({isLoading: true});
-			self.sell(elem);
-			}}>{self.state.isLoading ? 'Selling...' : 'Sell'} {this.state.quantity} of {elem.instr}</Button></td>
+                 <td>{elem.name} </td> <td>  {elem.amount} </td> <td> {elem.bp} </td> <td> {elem.ap} </td> 
+		
+		<td>
+		 <input type="number" 
+			value={this.state.sellQuantity[elem.name]} 
+			onChange={function (e) { self.handleSellValue(e, elem.name);}}>
+		</input>
+		<Button bsStyle="danger" bsSize="small" disabled={this.state.isLoading}
+		       	style={{marginLeft: 1 + 'em'}}
+		        onClick={function() {
+				self.setState({isLoading: true});
+				self.sell(elem);
+			}}>
+		  {self.state.isLoading ? 'Selling...' : 'Sell'} {this.state.sellQuantity[elem.name]} of {elem.instr}
+		</Button>
+		</td>
                  </tr>
                 
-                                              )}
+             )}
          </tbody>
         </Table>
         </div>
@@ -144,9 +164,9 @@ var Grid = React.createClass({
 							<input type="search" value={this.state.symbol} onChange={this.handleSymbol} placeholder="Eg AAPL:"></input>
 							<br></br>
 							<h4>Quantity below:</h4>
-							<input type="number" value={this.state.quantity} onChange={this.handleValue} placeholder="Quantity:"></input>
+							<input type="number" value={this.state.buyQuantity} onChange={this.handleBuyValue} placeholder="Quantity:"></input>
 							<br></br><br></br>
-							<Button bsStyle="success" onClick={!this.state.isLoading ? this.buyStocks : null}>{this.state.isLoading ? 'Buying... ' : 'Buy'} {this.state.quantity} of {this.state.symbol}</Button>	
+							<Button bsStyle="success" onClick={!this.state.isLoading ? this.buyStocks : null}>{this.state.isLoading ? 'Buying... ' : 'Buy'} {this.state.buyQuantity} of {this.state.symbol}</Button>	
 							</form>
                                                      </Modal.Body>          
                                                                  <Modal.Footer>
